bff7e7660757afb7074afb529d02d9de7c4b6c0e
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-03-03 11:19:54 +0200

    01 - CRUD on students table

---
+++ Assignment/Data/Models/ITIDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Data.Models
+{
+
+    class ITIDbContext : DbContext
+    {
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = ITI; Trusted_Connection = true; Encrypt = True; TrustServerCertificate = True");//Trust App To connect on sql server service throw Windows authentication.
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,80 @@
-﻿namespace Assignment
+﻿using Assignment.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using ITIDbContext dbContext = new ITIDbContext();
+
+            #region 01 - CRUD on students table 
+
+            #region 01.1 - ADD/Insert
+
+            //Student student = new Student()
+            //{
+            //    FName = "Eslam",
+            //    LName = "El-saadany",
+            //    Address = "Mansoura",
+            //    Age = 22,
+            //};
+            //Console.WriteLine(dbContext.Entry<Student>(student).State);//Detached
+            //dbContext.Add<Student>(student);
+            //Console.WriteLine(dbContext.Entry<Student>(student).State);//Added
+            //dbContext.SaveChanges();
+            //Console.WriteLine(dbContext.Entry<Student>(student).State);//Unchanged
+
+            #endregion
+
+            #region 01.2 - Retrieve
+
+            //var emp = dbContext.Students.AsNoTracking<Student>().FirstOrDefault(s => s.Age == 22);
+
+            //if (emp is not null)
+            //{
+
+            //    Console.WriteLine($"Id = {emp.Id} - FName = {emp.FName} - LName = {emp.LName} - Age = {emp.Age} - Address = {emp.Address}");
+            //    //Id = 2 - FName = Eslam - LName = El-saadany - Age = 22 - Address = Mansoura
+            //}
+
+            #endregion
+
+            #region 01.3 - Update
+
+            //var emp = dbContext.Students.FirstOrDefault(s => s.Age == 22);
+
+            //if (emp is not null)
+            //{
+
+            //    Console.WriteLine(dbContext.Entry<Student>(emp).State);//UnChanged
+            //    emp.Age = 33;
+            //    Console.WriteLine(dbContext.Entry<Student>(emp).State);//Modified
+            //    dbContext.SaveChanges();
+            //    Console.WriteLine(dbContext.Entry<Student>(emp).State);//UnChanged
+
+            //}
+
+            #endregion
+
+            #region 01.4 - Delete
+
+            //var emp = dbContext.Students.FirstOrDefault(s => s.Age == 22);
+
+            //if(emp is not null)
+            //{
+            //    Console.WriteLine(dbContext.Entry<Student>(emp).State);//UnChanged
+            //    dbContext.Remove<Student>(emp);
+            //    Console.WriteLine(dbContext.Entry<Student>(emp).State);//Deleted
+            //    dbContext.SaveChanges();
+            //    Console.WriteLine(dbContext.Entry<Student>(emp).State);//Detached
+
+            //}
+
+            #endregion
+
+            #endregion
         }
     }
 }
