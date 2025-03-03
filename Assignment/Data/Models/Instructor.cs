23f5e9988b3ba0eb6c5b390412bef4790c11864a
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-03-03 11:37:19 +0200

    .

---
+++ Assignment/Data/Models/Instructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Data.Models
+{
+    //Data Annotations
+    class Instructor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int InstId { get; set; }
+        [DataType("varchar(20)", ErrorMessage = "Maximum Length is 20 chars ")]
+        [MinLength(3, ErrorMessage = "Min Length is 3 chars")]
+        public string Name { get; set; }
+        public int Bonus { get; set; }
+        [DisplayName("Net_Salary")]
+        public int Salary { get; set; }
+        [DataType("varchar")]
+        [MaxLength(50)]
+        [MinLength(5)]
+        public string? Address { get; set; }
+        public decimal? HourRate { get; set; }
+        [NotMapped]
+        public int DeptId { get; set; }
+    }
+}
